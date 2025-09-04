f18060df61a87176d8e44e1c50ac09cea913b9ec
Anshika Bhadoria <anshikabhadoria88@gmail.com>
2025-09-04 13:13:40 +0530

    build

--- components/FAQInquirySection.jsx
+++ components/FAQInquirySection.jsx
@@ -256,7 +256,7 @@ const FAQInquirySection = () => {
           {/* Right Side - FAQs */}
           <div className="space-y-6">
             <h3 className="text-4xl font-bold text-gray-900 mb-6 py-6">
-              Frequently Asked Questions
+              Frequently Asked
             </h3>
 
             <div className="space-y-4">
