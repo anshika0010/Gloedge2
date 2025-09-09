4395ed43b881bb03da895d078e08c5923ea7f7f7
Anshika Bhadoria <anshikabhadoria88@gmail.com>
2025-09-09 15:49:22 +0530

    h

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,52 +1,27 @@
 "use client";
 
 import { useState, useEffect } from "react";
-
+import hero1 from "../images/new/hero1.jpeg";
+import hero2 from "../images/new/hero2.jpeg";
+import hero3 from "../images/new/hero3.jpeg";
 export default function HeroSection() {
   const serviceColors = {
     yellow: "bg-green-600 hover:bg-green-400",
     purple: "bg-blue-400 hover:bg-blue-600",
     blue: "bg-[#009bad] hover:bg-[#009bad]",
     green: "bg-yellow-400 hover:bg-yellow-600",
-    pink: "bg-pink-500 hover:bg-pink-600",
-    red: "bg-red-500 hover:bg-red-600",
+    pink: "bg-blue-800 hover:bg-blue-600",
+    red: "bg-green-800 hover:bg-green-600",
   };
   const tours = [
     {
-      title: "Eligibility Check",
-      desc: "Check your chances of qualifying for immigration easily.",
-      img: "https://i.pinimg.com/736x/02/e6/c0/02e6c06fb4087b1e7a874869e6ede6a0.jpg",
-      link: "/eligibility-check",
+      img: hero1,
     },
     {
-      title: "Skilled Migration",
-      desc: "Explore opportunities to move abroad with skilled migration programs.",
-      img: "https://i.pinimg.com/1200x/2f/6f/c3/2f6fc39117084474826749ad3e5f0417.jpg",
-      link: "/skilled-migration",
+      img: hero2,
     },
     {
-      title: "Jobs",
-      desc: "Find global job opportunities that match your skills.",
-      img: "https://images.unsplash.com/photo-1504384308090-c894fdcc538d?auto=format&fit=crop&w=1600&q=80",
-      link: "/jobs",
-    },
-    {
-      title: "Business and Investment",
-      desc: "Unlock pathways through business and investment visas.",
-      img: "https://i.pinimg.com/1200x/bf/e6/fd/bfe6fd81cfb210eedf4d04571389a0fa.jpg",
-      link: "/business-investment",
-    },
-    {
-      title: "Study",
-      desc: "Pursue your education in top global universities.",
-      img: "https://i.pinimg.com/1200x/e8/e0/b3/e8e0b3f8474647e651b8cd1d22e996f6.jpg",
-      link: "/study",
-    },
-    {
-      title: "Language Training",
-      desc: "Enhance your skills with professional language training.",
-      img: "https://i.pinimg.com/1200x/3c/ff/00/3cff006a9934d986ca014f7de735f7fc.jpg",
-      link: "/language-training",
+      img: hero3,
     },
   ];
 
@@ -66,13 +41,12 @@ export default function HeroSection() {
 
   return (
     <div
-      className="relative bg-gray-900 text-white flex flex-col  min-h-[500px]  lg:flex-row bg-cover bg-center transition-all duration-700 ease-in-out "
+      className="relative bg-gray-900 text-white flex flex-col min-h-[500px] lg:flex-row bg-cover bg-center transition-all duration-700 ease-in-out"
       style={{
-        backgroundImage: `url(${tours[slideIndex].img})`,
+        backgroundImage: `url(${tours[slideIndex].img.src})`,
       }}
     >
       {/* Overlay */}
-      <div className="absolute inset-0 bg-black/50 z-0"></div>
       {/* Left Hero Section */}
       <div className="relative z-10 flex-1 flex flex-col justify-between px-4 sm:px-6 md:px-10 lg:px-14 py-12 md:py-20 text-center lg:text-left">
         {/* Navigation */}
