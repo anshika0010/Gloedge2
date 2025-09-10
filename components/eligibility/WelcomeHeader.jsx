2e14a9a40bc5f041ad0f7dc4d820e2ddd3aa57e7
Anshika Bhadoria <anshikabhadoria88@gmail.com>
2025-09-10 12:29:47 +0530

    e

--- components/eligibility/WelcomeHeader.jsx
+++ components/eligibility/WelcomeHeader.jsx
@@ -1,16 +1,11 @@
 export default function WelcomeHeader() {
   return (
-    <div className="container mx-auto mt-2 border-gray-200  border p-6 bg-red-400">
-      <h4 className="text-lg font-medium mb-2">Hi,</h4>
-      <h1 className="text-2xl md:text-3xl font-bold mb-1">
+    <div className="container mx-auto mt-2 py-20 border-gray-200  border px-6 bg-red-600">
+      <h4 className="text-xl text-white font-medium mb-2">Hi,</h4>
+      <h1 className="text-2xl md:text-3xl text-white font-bold mb-1">
         Welcome to Your Free and Quick Wizard
       </h1>
-      <p className="text-white text-sm md:text-base">
-        Check your Eligibility
-      </p>
-
-      {/* Red underline accent */}
-      <div className="mt-4 w-20 h-2 bg-red-600"></div>
+      <p className="text-white text-sm md:text-base">Check your Eligibility</p>
     </div>
   );
 }
